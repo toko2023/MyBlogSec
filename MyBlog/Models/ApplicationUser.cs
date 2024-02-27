26c51c58168b0c88863e2423db5563aa11c9dd94
toko2023 <dorj2021zundui@gmail.com>
2024-02-27 16:16:51 +0800

    Add project files.

---
+++ MyBlog/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyBlog.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+        // relation
+        public List<Post>? Posts { get; set; } 
+        public List<Page>? Pages { get; set; }
+    }
+}
