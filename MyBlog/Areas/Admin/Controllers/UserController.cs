196d94ca8f7ffa6ce22c2e4fa0ec43ff5ae06968
toko2023 <dorj2021zundui@gmail.com>
2024-02-28 12:40:12 +0800

    login theme added

---
+++ MyBlog/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
