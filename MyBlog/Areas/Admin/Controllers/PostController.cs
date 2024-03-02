3c3bffd5ccd8e9b6b36935d60dc467dd4a1632cd
toko2023 <dorj2021zundui@gmail.com>
2024-03-02 17:51:13 +0800

    post create view

--- MyBlog/Areas/Admin/Controllers/PostController.cs
+++ MyBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.ViewModels;
 
 namespace MyBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class PostController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreatePostVM());
         }
     }
 }
---
+++ MyBlog/ViewModels/CreatePostVM.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Models;
+
+namespace MyBlog.ViewModels
+{
+    public class CreatePostVM
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ApplicationUserId { get; set; }
+        public string? Description { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public IFormFile? Thumbnail { get; set;}
+    }
+}
